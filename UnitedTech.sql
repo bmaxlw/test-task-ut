29cbb84322439a0215febadb13ca74a28f8e8f87
Maksym Bondaruk <b.max.lw@gmail.com>
2022-06-20 19:30:46 +0300

    [SQL]: Task #1 solution added...

---
+++ UnitedTech.sql
@@ -0,0 +1,25 @@
+-- MS SQL Server-based syntax used
+
+-- Task #1:
+-- If partition needed only by dates and countries are applied just as general a filter.
+SELECT [Date],
+       COUNT(DISTINCT userId) RealUsers
+FROM registration
+-- <WHERE> here works with <GROUP BY> since it filters 
+-- the <SELECT> stmt and not <GROUP BY> itself
+WHERE TestType = 1 
+      AND CAST([Date] AS NVARCHAR)
+      BETWEEN '2022-01-01' AND '2022-04-31'
+      AND Country IN ( 'US', 'GB', 'MX', 'CA', 'IE' )
+GROUP BY [Date];
+
+-- If partition needed both by dates and by countries
+SELECT [Date],
+       Country,
+       COUNT(userId) OVER (PARTITION BY [Date], Country) RealUsers
+FROM registration
+WHERE TestType = 1
+      AND CAST([Date] AS NVARCHAR)
+      BETWEEN '2022-01-01' AND '2022-04-31'
+      AND Country IN ( 'US', 'GB', 'MX', 'CA', 'IE' )
+ORDER BY [Date]
