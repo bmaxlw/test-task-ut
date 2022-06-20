b30239f13d90399b6a6f5fba715d93d9c6a7f8cc
Maksym Bondaruk <b.max.lw@gmail.com>
2022-06-20 21:13:42 +0300

    [SQL]: Task #4 solution added...

--- UnitedTech.sql
+++ UnitedTech.sql
@@ -6,7 +6,7 @@ SELECT [Date],
        COUNT(DISTINCT userId) RealUsers
 FROM registration
 -- <WHERE> works with <GROUP BY> in this case since it filters 
--- the <SELECT> statement and not <GROUP BY> itself
+-- the <SELECT> statement and not <GROUP BY> itself...
 WHERE TestType = 1 
       AND CAST([Date] AS NVARCHAR)
       BETWEEN '2022-01-01' AND '2022-04-31'
@@ -24,6 +24,7 @@ WHERE TestType = 1
       AND Country IN ( 'US', 'GB', 'MX', 'CA', 'IE' )
 ORDER BY [Date]
 
+
 -- Task #2:
 SELECT DISTINCT
     [Date],
@@ -34,6 +35,7 @@ WHERE TestType = 1
       AND CAST([Date] AS NVARCHAR)
       BETWEEN '2021-01-01' AND '2021-12-31';
 
+
 -- Task #3:
 SELECT DISTINCT
     Country,
@@ -42,3 +44,29 @@ FROM Payments
 WHERE TestType = 1
       AND TransactionStatus = 2
       AND Country IN ( 'US', 'GB', 'MX', 'CA', 'PL' );
+
+
+-- Task #4:
+-- "на підзапит" <- where a subquery supposed to be used ???
+SELECT userId,
+       SUM(PriceAmount * ExchangeCurrencyRate) TotalPerUser
+FROM Payments
+-- <WHERE> works with <GROUP BY> in this case since it filters 
+-- the <SELECT> statement and not <GROUP BY> itself...
+WHERE TestType = 1 
+      AND TransactionStatus = 2
+GROUP BY userId
+HAVING SUM(PriceAmount * ExchangeCurrencyRate) > 1000
+
+-- Variant with CTE
+WITH cte
+AS (SELECT userId,
+           SUM(PriceAmount * ExchangeCurrencyRate) TotalPerUser
+    FROM Payments
+    WHERE TestType = 1
+          AND TransactionStatus = 2
+    GROUP BY userId
+   )
+SELECT *
+FROM cte
+WHERE TotalPerUser > 1000;
